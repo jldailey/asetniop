abe14b62ef23265b321ad4d38ff5cff66ef38459
Jesse Dailey <jesse.dailey@gmail.com>
2012-11-26 18:45:54 -0500

    trivial formatting

--- android/src/org/asetniop/ImePreferences.java
+++ android/src/org/asetniop/ImePreferences.java
@@ -26,31 +26,31 @@ import com.example.android.softkeyboard.R;
  * Displays the IME preferences inside the input method setting.
  */
 public class ImePreferences extends PreferenceActivity {
-    @Override
-    public Intent getIntent() {
-        final Intent modIntent = new Intent(super.getIntent());
-        modIntent.putExtra(EXTRA_SHOW_FRAGMENT, Settings.class.getName());
-        modIntent.putExtra(EXTRA_NO_HEADERS, true);
-        return modIntent;
-    }
+	@Override
+	public Intent getIntent() {
+		final Intent modIntent = new Intent(super.getIntent());
+		modIntent.putExtra(EXTRA_SHOW_FRAGMENT, Settings.class.getName());
+		modIntent.putExtra(EXTRA_NO_HEADERS, true);
+		return modIntent;
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        // We overwrite the title of the activity, as the default one is "Voice Search".
-        setTitle(R.string.settings_name);
-    }
+		// We overwrite the title of the activity, as the default one is "Voice Search".
+		setTitle(R.string.settings_name);
+	}
 
-    public static class Settings extends InputMethodSettingsFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setInputMethodSettingsCategoryTitle(R.string.language_selection_title);
-            setSubtypeEnablerTitle(R.string.select_language);
+	public static class Settings extends InputMethodSettingsFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setInputMethodSettingsCategoryTitle(R.string.language_selection_title);
+			setSubtypeEnablerTitle(R.string.select_language);
 
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.ime_preferences);
-        }
-    }
+			// Load the preferences from an XML resource
+			addPreferencesFromResource(R.xml.ime_preferences);
+		}
+	}
 }
